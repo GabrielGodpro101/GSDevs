53ff6dd024eae2b990127172656f9913e322a82b
GabrielGodpro101 <48134914+GabrielGodpro101@users.noreply.github.com>
2020-01-11 09:49:47 -0500

    Alpha Pre-Release 0.9

---
+++ CPDDirect.bat
@@ -0,0 +1,47 @@
+@echo off
+title CPDDirect- Welcome To CPDDirect
+color 6
+echo. Welcome To CPDDirect
+echo.
+echo. By GoldTech
+echo www.goldentech.com
+pause
+title CPDDirect
+cls
+color a
+dir
+pause
+tree
+pause
+ipconfig
+pause
+tasklist
+pause
+systeminfo
+pause
+ver
+pause
+vol
+pause
+wmic product get name
+pause
+echo.
+echo Press ENTER To Keep The Current Time
+echo.
+pause
+time
+pause
+echo.
+echo Press ENTER To Keep The Current Date
+echo.
+pause
+date
+cls
+echo. Your Info Has Been Cleared
+echo. Press Any Key To Close
+pause
+cls
+echo Thank You For Using CPDDIR
+echo.
+echo The Program Will Close Automaticly
+PING 3.0  -n  3.0 -w 3.0 >NUL
